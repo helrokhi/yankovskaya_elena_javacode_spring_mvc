91640f825fda39579153337026d5e563d9c68451
e.yankovskaya <elena.parokhina@gmail.com>
2025-09-15 15:24:05 +0200

    update mapper

--- src/main/java/ru/pro/mapper/AuthorMapper.java
+++ src/main/java/ru/pro/mapper/AuthorMapper.java
@@ -1,15 +1,10 @@
 package ru.pro.mapper;
 
-import org.mapstruct.BeanMapping;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
-import org.mapstruct.MappingTarget;
 import ru.pro.model.dto.AuthorDto;
 import ru.pro.model.entity.AuthorEntity;
 
 import java.util.List;
-
-import static org.mapstruct.NullValuePropertyMappingStrategy.IGNORE;
 
 @Mapper(componentModel = "spring")
 public interface AuthorMapper {
@@ -18,8 +13,4 @@ public interface AuthorMapper {
     AuthorEntity toEntity(AuthorDto dto);
 
     List<AuthorDto> toDtoList(List<AuthorEntity> entities);
-
-    @BeanMapping(nullValuePropertyMappingStrategy = IGNORE)
-    @Mapping(target = "id", ignore = true)
-    void updateEntity(AuthorDto source, @MappingTarget AuthorEntity target);
 }
